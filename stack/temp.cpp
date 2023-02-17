6db9302c6b6298906d18c5b2e906b3dce00aaf51
Aniket kumar sahu <91835556+aniketsahu23@users.noreply.github.com>
2023-02-17 23:43:52 +0530

    update

--- stack/temp.cpp
+++ stack/temp.cpp
@@ -1,16 +1,45 @@
-#include<bits/stdc++.h>
-using namespace std;
-const int a=11;
-const int b=10;
-int main(){
-    vector<int> nums(5);
-    nums={2,2,3,3,-1};
-        vector<int> v1(nums.size()+1,0);
-        for(int i=0;i<nums.size();i++){
-            v1[nums[i]]++;
+#include <stdio.h>
+
+#define FLAG 0x7E // Flag byte value
+#define ESCAPE 0x7D // Escape byte value
+
+// Function to stuff bits
+void stuffBits(unsigned char* data, int* length) {
+    int count = 0;
+    for (int i = 0; i < *length; i++) {
+        if (data[i] == FLAG || data[i] == ESCAPE) {
+            count++;
         }
-        for(int i=0;i<v1.size();i++){
-            cout<<v1[i]<<" "; 
+    }
+    *length += count;
+
+    for (int i = *length - 1; i >= 0; i--) {
+        if (data[i - count] == FLAG || data[i - count] == ESCAPE) {
+            data[i] = data[i - count];
+            data[i - 1] = ESCAPE;
+            count--;
         }
+        else {
+            data[i] = data[i - count];
+        }
+    }
+}
+
+int main() {
+    unsigned char data[10] = {0x7E, 0x81, 0x82, 0x7D, 0x83, 0x84, 0x7E, 0x85, 0x86, 0x7E};
+    int length = 10;
+
+    printf("Original data: ");
+    for (int i = 0; i < length; i++) {
+        printf("%02X ", data[i]);
+    }
+
+    stuffBits(data, &length);
+
+    printf("\nStuffed data: ");
+    for (int i = 0; i < length; i++) {
+        printf("%02X ", data[i]);
+    }
+
     return 0;
 }
