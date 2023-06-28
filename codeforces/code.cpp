a530e8beb5b377f590c150297a7c53289b3d212d
aniket <aniketsahu2317@gmail.com>
2023-06-28 23:39:37 +0530

    update

--- codeforces/code.cpp
+++ codeforces/code.cpp
@@ -1,32 +1,75 @@
-#include<bits/stdc++.h>
-#define ll long long int   
-#define ld long double
-#define vi vector<ll>
-#define pii pair<int, int>
-#define fi first
-#define se second
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
-#define fill(a, b) memset(a, b, sizeof(a))
-#define inf 1000000000
-#define binf 1e18 + 100
-#define rep(i,a,b) for(int i=a;i<b;i++)
+/*----------Flying Dutchman----------*/
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#define vi vector<int>
+#define rep(i,a,b) for(int i = a; i < b; i++)
+// #define ll long long int
 using namespace std;
-void solve() {
-    ll k,n,w;
-    cin>>k>>n>>w;
-    ll sum=0;
-    for(int i=1;i<=w;i++){
-        sum+=k*i;
-    }
-    ll ans=n-sum;
-    cout<<((ans >= 0) ? 0 : abs(ans))<<endl;
+long long mod = 1e9 + 7;
+long long INF = 1e7;
+double EPS = 1e-12;
+typedef long long int ll;
+
+inline ll sc() { ll a; scanf("%lld", &a); return a; }
+inline int scd() { int a; scanf("%d", &a); return a; }
+#define all(c) (c).begin(), (c).end()
+#define sz(a) ((int)a.size())
+ 
+#define popcount __builtin_popcountll
+ 
+typedef pair<ll,ll> PA;
+bool bitSet(ll n, ll bit){
+    if((n & (1LL << bit)) != 0) return true;
+    else return false;
 }
 
-int main() {
-    // int t;
-    // cin >> t;
-    // while (t--) 
+vector<vector<int>> g;
+vector<ll> cnt;
+ 
+void dfs(int v, int p) {
+    if (g[v].size() == 1 && g[v][0] == p) {
+        cnt[v] = 1;
+    } else {
+        for (auto u : g[v]) {
+            if (u != p) {
+                dfs(u, v);
+                cnt[v] += cnt[u];
+            }
+        }
+    }
+}
+ 
+void solve() {
+    int n, q;
+    cin >> n;
+ 
+    g.assign(n, vector<int>());
+    
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+ 
+    cnt.assign(n, 0);
+    dfs(0, -1);
+ 
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int c, k;
+        cin >> c >> k;
+        c--; k--;
+ 
+        ll res = cnt[c] * cnt[k];
+        cout << res << '\n';
+    }
+}
+ 
+int main(){
+    int t = scd();
+    while(t--) 
         solve();
     return 0;
 }
--- codeforces/gaming.cpp
+++ codeforces/gaming.cpp
@@ -1,25 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+// #ifdef Flying Dutchman
 using namespace std;
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        vector<int> v(n);
-        unordered_map<int,int> mp; 
-        for(int i=0;i<n;i++){
-            cin>>v[i];
-            mp[v[i]]++;
-        }
-        int cnt=0;
-        for(auto x:mp){
-            if(x.first == 1){
-                cnt+=(x.second%2==0) ? x.second/2 : x.second/2 + 1;
-            }else
-                cnt+=x.second;
-        }
-        cout<<cnt<<endl;
+long long mod = 1e9 + 7;
+long long INF = 1e7;
+double EPS = 1e-12;
+typedef long long int ll;
+#define rep(i,a,n) for(int i = a; i < n; i++)
+inline ll sc() { ll a; scanf("%lld", &a); return a; }
+inline int scd() { int a; scanf("%d", &a); return a; }
+#define all(c) (c).begin(), (c).end()
+#define sz(a) ((int)a.size())
+
+#define popcount __builtin_popcountll
+
+typedef pair<ll,ll> PA;
+bool bitSet(ll n, ll bit){
+    if((n & (1LL << bit)) != 0) return true;
+    else return false;
+}
+#define lim 2000010
+#define lim2 200010
+#define qqqqqq 210
+string S[lim];
+ll A[lim];
+
+void codeforces(){
+    int n; cin >> n;
+    int mn = INT_MAX, mx = INT_MIN;
+    rep(i,0,n) {
+        int x; cin >> x;
+        mn = min(mn, x);
+        mx = max(mx, x);
     }
+    cout << ((mn < 0) ? mn : mx) << endl;
+}
+
+int main(){
+    int t = scd();
+    while(t--) 
+        codeforces();
     return 0;
 }
